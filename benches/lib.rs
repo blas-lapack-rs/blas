32f121e4386070af48fb1adcb1935466210e8f80
Ivan Ukhov <ivan.ukhov@gmail.com>
2014-10-06 14:19:25 +0200

    Moved tests into /tests and benches into /benches

---
+++ benches/lib.rs
@@ -0,0 +1,34 @@
+extern crate blas;
+extern crate test;
+
+use test::Bencher;
+
+#[bench]
+fn dgemv_few_large(b: &mut Bencher) {
+    let m = 1000;
+
+    let a = Vec::from_elem(m * m, 1.0);
+    let x = Vec::from_elem(m * 1, 1.0);
+    let mut y = Vec::from_elem(m * 1, 1.0);
+
+    b.iter(|| {
+        blas::dgemv(blas::NORMAL, m as i32, m as i32, 1.0, a.as_ptr(),
+            m as i32, x.as_ptr(), 1, 1.0, y.as_mut_ptr(), 1)
+    })
+}
+
+#[bench]
+fn dgemv_many_small(b: &mut Bencher) {
+    let m = 20;
+
+    let a = Vec::from_elem(m * m, 1.0);
+    let x = Vec::from_elem(m * 1, 1.0);
+    let mut y = Vec::from_elem(m * 1, 1.0);
+
+    b.iter(|| {
+        for _ in range(0u, 20000) {
+            blas::dgemv(blas::NORMAL, m as i32, m as i32, 1.0, a.as_ptr(),
+                m as i32, x.as_ptr(), 1, 1.0, y.as_mut_ptr(), 1);
+        }
+    })
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,3 @@
-#![feature(macro_rules)]
-
 extern crate libc;
 
 use libc::{c_char, c_double, c_int};
@@ -37,81 +35,5 @@ pub fn dgemm(transa: i8, transb: i8, m: i32, n: i32, k: i32, alpha: f64, a: *con
 
     unsafe {
         dgemm_(&transa, &transb, &m, &n, &k, &alpha, a, &lda, b, &ldb, &beta, c, &ldc);
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    extern crate test;
-
-    use self::test::Bencher;
-
-    macro_rules! assert_equal(
-        ($given:expr , $expected:expr) => ({
-            assert_eq!($given.len(), $expected.len());
-            for i in range(0u, $given.len()) {
-                assert_eq!($given[i], $expected[i]);
-            }
-        });
-    )
-
-    #[test]
-    fn dgemv() {
-        let (m, n) = (2, 3);
-
-        let a = vec![1.0, 4.0, 2.0, 5.0, 3.0, 6.0];
-        let x = vec![1.0, 2.0, 3.0];
-        let mut y = vec![6.0, 8.0];
-
-        super::dgemv(super::NORMAL, m, n, 1.0, a.as_ptr(), m, x.as_ptr(), 1,
-            1.0, y.as_mut_ptr(), 1);
-
-        let expected_y = vec![20.0, 40.0];
-        assert_equal!(y, expected_y);
-    }
-
-    #[test]
-    fn dgemm() {
-        let (m, n, k) = (2, 4, 3);
-
-        let a = vec![1.0, 4.0, 2.0, 5.0, 3.0, 6.0];
-        let b = vec![1.0, 5.0, 9.0, 2.0, 6.0, 10.0, 3.0, 7.0, 11.0, 4.0, 8.0, 12.0];
-        let mut c = vec![2.0, 7.0, 6.0, 2.0, 0.0, 7.0, 4.0, 2.0];
-
-        super::dgemm(super::NORMAL, super::NORMAL, m, n, k, 1.0, a.as_ptr(),
-            m, b.as_ptr(), k, 1.0, c.as_mut_ptr(), m);
-
-        let expected_c = vec![40.0, 90.0, 50.0, 100.0, 50.0, 120.0, 60.0, 130.0];
-        assert_equal!(c, expected_c);
-    }
-
-    #[bench]
-    fn dgemv_few_large(b: &mut Bencher) {
-        let m = 1000;
-
-        let a = Vec::from_elem(m * m, 1.0);
-        let x = Vec::from_elem(m * 1, 1.0);
-        let mut y = Vec::from_elem(m * 1, 1.0);
-
-        b.iter(|| {
-            super::dgemv(super::NORMAL, m as i32, m as i32, 1.0, a.as_ptr(),
-                m as i32, x.as_ptr(), 1, 1.0, y.as_mut_ptr(), 1)
-        })
-    }
-
-    #[bench]
-    fn dgemv_many_small(b: &mut Bencher) {
-        let m = 20;
-
-        let a = Vec::from_elem(m * m, 1.0);
-        let x = Vec::from_elem(m * 1, 1.0);
-        let mut y = Vec::from_elem(m * 1, 1.0);
-
-        b.iter(|| {
-            for _ in range(0u, 20000) {
-                super::dgemv(super::NORMAL, m as i32, m as i32, 1.0, a.as_ptr(),
-                    m as i32, x.as_ptr(), 1, 1.0, y.as_mut_ptr(), 1);
-            }
-        })
     }
 }
