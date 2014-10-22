9d65af9aab0b17db708fd3518e4d57ebf2db05d6
Ivan Ukhov <ivan.ukhov@gmail.com>
2014-10-22 08:24:37 +0400

    Cosmetic changes

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-//! The library provides an interface to the [Basic Linear Algebra Subprograms][1].
+//! An interface to the [Basic Linear Algebra Subprograms][1].
 //!
 //! [1]: http://www.netlib.org/blas/
 
@@ -9,28 +9,26 @@ use libc::{c_char, c_double, c_int};
 #[link(name = "gfortran")]
 #[link(name = "blas", kind = "static")]
 extern {
-    fn dgemv_(trans: *const c_char, m: *const c_int, n: *const c_int,
-              alpha: *const c_double, a: *const c_double, lda: *const c_int,
-              x: *const c_double, incx: *const c_int, beta: *const c_double,
-              y: *mut c_double, incy: *const c_int);
+    fn dgemv_(trans: *const c_char, m: *const c_int, n: *const c_int, alpha: *const c_double,
+              a: *const c_double, lda: *const c_int, x: *const c_double, incx: *const c_int,
+              beta: *const c_double, y: *mut c_double, incy: *const c_int);
 
-    fn dgemm_(transa: *const c_char, transb: *const c_char, m: *const c_int,
-              n: *const c_int, k: *const c_int, alpha: *const c_double,
-              a: *const c_double, lda: *const c_int, b: *const c_double,
-              ldb: *const c_int, beta: *const c_double, c: *mut c_double,
+    fn dgemm_(transa: *const c_char, transb: *const c_char, m: *const c_int, n: *const c_int,
+              k: *const c_int, alpha: *const c_double, a: *const c_double, lda: *const c_int,
+              b: *const c_double, ldb: *const c_int, beta: *const c_double, c: *mut c_double,
               ldc: *const c_int);
 }
 
-/// Performs a matrix-vector operation.
+/// Performs a matrix-vector multiplication followed by an addition.
 ///
 /// The function performs one of the matrix-vector operations
 ///
-/// ```ignore
-/// y := alpha * a * x + beta * y or
-/// y := alpha * a^T * x + beta * y
+/// ```math
+/// y := alpha * A * x + beta * y or
+/// y := alpha * A^T * x + beta * y
 /// ```
 ///
-/// where `alpha` and `beta` are scalars, `x` and `y` are vectors, and `a` is
+/// where `alpha` and `beta` are scalars, `x` and `y` are vectors, and `A` is
 /// an `m`-by-`n` matrix.
 ///
 /// http://www.netlib.org/lapack/explore-html/dc/da8/dgemv_8f.html
@@ -39,39 +37,37 @@ pub fn dgemv(trans: u8, m: uint, n: uint, alpha: f64, a: *const f64, lda: uint,
              x: *const f64, incx: uint, beta: f64, y: *mut f64, incy: uint) {
 
     unsafe {
-        dgemv_(&(trans as i8), &(m as i32), &(n as i32), &alpha, a,
-               &(lda as i32), x, &(incx as i32), &beta, y, &(incy as i32));
+        dgemv_(&(trans as i8), &(m as i32), &(n as i32), &alpha, a, &(lda as i32),
+               x, &(incx as i32), &beta, y, &(incy as i32));
     }
 }
 
-/// Performs a matrix-matrix operation.
+/// Performs a matrix-matrix multiplication followed by an addition.
 ///
 /// The function performs one of the matrix-matrix operations
 ///
-/// ```ignore
-/// c := alpha * op(a) * op(b) + beta * c
+/// ```math
+/// C := alpha * op(A) * op(B) + beta * C
 /// ```
 ///
-/// where `op(x)` is one of
+/// where `op(X)` is one of
 ///
-/// ```ignore
-/// op(x) = x or
-/// op(x) = x^T,
+/// ```math
+/// op(X) = X or
+/// op(X) = X^T,
 /// ```
 ///
-/// `alpha` and `beta` are scalars, and `a`, `b`, and `c` are matrices, with
-/// `op(a)` an `m`-by-`k` matrix, `op(b)` a `k`-by-`n` matrix, and `c` an
+/// `alpha` and `beta` are scalars, and `A`, `B`, and `C` are matrices, with
+/// `op(A)` an `m`-by-`k` matrix, `op(B)` a `k`-by-`n` matrix, and `C` an
 /// `m`-by-`n` matrix.
 ///
 /// http://www.netlib.org/lapack/explore-html/d7/d2b/dgemm_8f.html
 #[inline]
-pub fn dgemm(transa: u8, transb: u8, m: uint, n: uint, k: uint, alpha: f64,
-             a: *const f64, lda: uint, b: *const f64, ldb: uint, beta: f64,
-             c: *mut f64, ldc: uint) {
+pub fn dgemm(transa: u8, transb: u8, m: uint, n: uint, k: uint, alpha: f64, a: *const f64,
+             lda: uint, b: *const f64, ldb: uint, beta: f64, c: *mut f64, ldc: uint) {
 
     unsafe {
-        dgemm_(&(transa as i8), &(transb as i8), &(m as i32), &(n as i32),
-               &(k as i32), &alpha, a, &(lda as i32), b, &(ldb as i32), &beta,
-               c, &(ldc as i32));
+        dgemm_(&(transa as i8), &(transb as i8), &(m as i32), &(n as i32), &(k as i32),
+               &alpha, a, &(lda as i32), b, &(ldb as i32), &beta, c, &(ldc as i32));
     }
 }
