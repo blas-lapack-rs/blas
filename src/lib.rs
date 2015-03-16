4101d48d7b0225a4230d9f59e56c323f0310bc0e
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-03-16 18:29:01 +0100

    Switch to the new version of libblas-sys

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,6 @@
 
 #![cfg_attr(test, feature(test))]
 
-#![allow(non_snake_case)]
-
 #[cfg(test)]
 #[macro_use]
 extern crate assert;
@@ -15,27 +13,39 @@ extern crate test;
 
 extern crate "libblas-sys" as raw;
 
-/// http://www.netlib.org/lapack/explore-html/dc/da8/dgemv_8f.html
+pub enum Layout {
+    RowMajor = raw::CblasRowMajor as isize,
+    ColumnMajor = raw::CblasColMajor as isize,
+}
+
+pub enum Transpose {
+    None = raw::CblasNoTrans as isize,
+    Transpose = raw::CblasTrans as isize,
+    ConjugateTranspose = raw::CblasConjTrans as isize,
+}
+
 #[inline]
-pub fn dgemv(TRANS: u8, M: usize, N: usize, ALPHA: f64, A: &[f64], LDA: usize,
-             X: &[f64], INCX: usize, BETA: f64, Y: &mut [f64], INCY: usize) {
+pub fn dgemv(layout: Layout, trans: Transpose, m: usize, n: usize, alpha: f64,
+             a: &[f64], lda: usize, x: &[f64], incx: usize, beta: f64,
+             y: &mut [f64], incy: usize) {
 
     unsafe {
-        raw::dgemv(&(TRANS as i8), &(M as i32), &(N as i32), &ALPHA,
-                   A.as_ptr(), &(LDA as i32), X.as_ptr(), &(INCX as i32),
-                   &BETA, Y.as_mut_ptr(), &(INCY as i32));
+        raw::cblas_dgemv(layout as u32, trans as u32, m as i32, n as i32, alpha,
+                         a.as_ptr(), lda as i32, x.as_ptr(), incx as i32, beta,
+                         y.as_mut_ptr(), incy as i32);
     }
 }
 
-/// http://www.netlib.org/lapack/explore-html/d7/d2b/dgemm_8f.html
 #[inline]
-pub fn dgemm(TRANSA: u8, TRANSB: u8, M: usize, N: usize, K: usize, ALPHA: f64, A: &[f64],
-             LDA: usize, B: &[f64], LDB: usize, BETA: f64, C: &mut [f64], LDC: usize) {
+pub fn dgemm(layout: Layout, transa: Transpose, transb: Transpose, m: usize,
+             n: usize, k: usize, alpha: f64, a: &[f64], lda: usize, b: &[f64],
+             ldb: usize, beta: f64, c: &mut [f64], ldc: usize) {
 
     unsafe {
-        raw::dgemm(&(TRANSA as i8), &(TRANSB as i8), &(M as i32), &(N as i32),
-                   &(K as i32), &ALPHA, A.as_ptr(), &(LDA as i32), B.as_ptr(),
-                   &(LDB as i32), &BETA, C.as_mut_ptr(), &(LDC as i32));
+        raw::cblas_dgemm(layout as u32, transa as u32, transb as u32,
+                         m as i32, n as i32, k as i32, alpha, a.as_ptr(),
+                         lda as i32, b.as_ptr(), ldb as i32, beta,
+                         c.as_mut_ptr(), ldc as i32);
     }
 }
 
@@ -43,30 +53,32 @@ pub fn dgemm(TRANSA: u8, TRANSB: u8, M: usize, N: usize, K: usize, ALPHA: f64, A
 mod tests {
     #[test]
     fn dgemv() {
-        let (M, N) = (2, 3);
+        let (m, n) = (2, 3);
 
-        let A = vec![1.0, 4.0, 2.0, 5.0, 3.0, 6.0];
-        let X = vec![1.0, 2.0, 3.0];
-        let mut Y = vec![6.0, 8.0];
+        let a = vec![1.0, 4.0, 2.0, 5.0, 3.0, 6.0];
+        let x = vec![1.0, 2.0, 3.0];
+        let mut y = vec![6.0, 8.0];
 
-        ::dgemv(b'N', M, N, 1.0, &A, M, &X, 1, 1.0, &mut Y, 1);
+        ::dgemv(::Layout::ColumnMajor, ::Transpose::None,
+                m, n, 1.0, &a, m, &x, 1, 1.0, &mut y, 1);
 
-        let expected_Y = vec![20.0, 40.0];
-        assert_equal!(Y, expected_Y);
+        let expected_y = vec![20.0, 40.0];
+        assert_equal!(y, expected_y);
     }
 
     #[test]
     fn dgemm() {
-        let (M, N, K) = (2, 4, 3);
+        let (m, n, k) = (2, 4, 3);
 
-        let A = vec![1.0, 4.0, 2.0, 5.0, 3.0, 6.0];
-        let B = vec![1.0, 5.0, 9.0, 2.0, 6.0, 10.0, 3.0, 7.0, 11.0, 4.0, 8.0, 12.0];
-        let mut C = vec![2.0, 7.0, 6.0, 2.0, 0.0, 7.0, 4.0, 2.0];
+        let a = vec![1.0, 4.0, 2.0, 5.0, 3.0, 6.0];
+        let b = vec![1.0, 5.0, 9.0, 2.0, 6.0, 10.0, 3.0, 7.0, 11.0, 4.0, 8.0, 12.0];
+        let mut c = vec![2.0, 7.0, 6.0, 2.0, 0.0, 7.0, 4.0, 2.0];
 
-        ::dgemm(b'N', b'N', M, N, K, 1.0, &A, M, &B, K, 1.0, &mut C, M);
+        ::dgemm(::Layout::ColumnMajor, ::Transpose::None, ::Transpose::None,
+                m, n, k, 1.0, &a, m, &b, k, 1.0, &mut c, m);
 
-        let expected_C = vec![40.0, 90.0, 50.0, 100.0, 50.0, 120.0, 60.0, 130.0];
-        assert_equal!(C, expected_C);
+        let expected_c = vec![40.0, 90.0, 50.0, 100.0, 50.0, 120.0, 60.0, 130.0];
+        assert_equal!(c, expected_c);
     }
 }
 
@@ -76,28 +88,30 @@ mod benches {
 
     #[bench]
     fn dgemv_few_large(bench: &mut ::test::Bencher) {
-        let M = 1000;
+        let m = 1000;
 
-        let A = repeat(1.0).take(M * M).collect::<Vec<_>>();
-        let X = repeat(1.0).take(M * 1).collect::<Vec<_>>();
-        let mut Y = repeat(1.0).take(M * 1).collect::<Vec<_>>();
+        let a = repeat(1.0).take(m * m).collect::<Vec<_>>();
+        let x = repeat(1.0).take(m).collect::<Vec<_>>();
+        let mut y = repeat(1.0).take(m).collect::<Vec<_>>();
 
         bench.iter(|| {
-            ::dgemv(b'N', M, M, 1.0, &A, M, &X, 1, 1.0, &mut Y, 1)
+            ::dgemv(::Layout::ColumnMajor, ::Transpose::None,
+                    m, m, 1.0, &a, m, &x, 1, 1.0, &mut y, 1)
         });
     }
 
     #[bench]
     fn dgemv_many_small(bench: &mut ::test::Bencher) {
-        let M = 20;
+        let m = 20;
 
-        let A = repeat(1.0).take(M * M).collect::<Vec<_>>();
-        let X = repeat(1.0).take(M * 1).collect::<Vec<_>>();
-        let mut Y = repeat(1.0).take(M * 1).collect::<Vec<_>>();
+        let a = repeat(1.0).take(m * m).collect::<Vec<_>>();
+        let x = repeat(1.0).take(m).collect::<Vec<_>>();
+        let mut y = repeat(1.0).take(m).collect::<Vec<_>>();
 
         bench.iter(|| {
             for _ in 0..20000 {
-                ::dgemv(b'N', M, M, 1.0, &A, M, &X, 1, 1.0, &mut Y, 1);
+                ::dgemv(::Layout::ColumnMajor, ::Transpose::None,
+                        m, m, 1.0, &a, m, &x, 1, 1.0, &mut y, 1);
             }
         });
     }
