4ab3f32bd1bd4b255f616e7fb734d916dabcf97a
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-07-22 14:27:52 -0400

    Turn the test into an example

--- README.md
+++ README.md
@@ -4,6 +4,20 @@ The package provides an interface to the [Basic Linear Algebra Subprograms][1].
 
 ## [Documentation][docs]
 
+## Example
+
+```rust
+let (m, n, k) = (2, 4, 3);
+
+let a = vec![1.0, 4.0, 2.0, 5.0, 3.0, 6.0];
+let b = vec![1.0, 5.0, 9.0, 2.0, 6.0, 10.0, 3.0, 7.0, 11.0, 4.0, 8.0, 12.0];
+let mut c = vec![2.0, 7.0, 6.0, 2.0, 0.0, 7.0, 4.0, 2.0];
+
+blas::dgemm(b'N', b'N', m, n, k, 1.0, &a, m, &b, k, 1.0, &mut c, m);
+
+assert_eq!(&c, &vec![40.0, 90.0, 50.0, 100.0, 50.0, 120.0, 60.0, 130.0]);
+```
+
 ## Contributing
 
 1. Fork the project.
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,19 @@
 //! Interface to the [Basic Linear Algebra Subprograms][1].
 //!
+//! ## Example
+//!
+//! ```
+//! let (m, n, k) = (2, 4, 3);
+//!
+//! let a = vec![1.0, 4.0, 2.0, 5.0, 3.0, 6.0];
+//! let b = vec![1.0, 5.0, 9.0, 2.0, 6.0, 10.0, 3.0, 7.0, 11.0, 4.0, 8.0, 12.0];
+//! let mut c = vec![2.0, 7.0, 6.0, 2.0, 0.0, 7.0, 4.0, 2.0];
+//!
+//! blas::dgemm(b'N', b'N', m, n, k, 1.0, &a, m, &b, k, 1.0, &mut c, m);
+//!
+//! assert_eq!(&c, &vec![40.0, 90.0, 50.0, 100.0, 50.0, 120.0, 60.0, 130.0]);
+//! ```
+//!
 //! [1]: http://en.wikipedia.org/wiki/Basic_Linear_Algebra_Subprograms
 
 extern crate blas_sys as ffi;
