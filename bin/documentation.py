1db88385ac8f92f9e37b5d0e7109c3e12f279a45
Ivan Ukhov <ivan.ukhov@gmail.com>
2016-11-20 13:22:10 +0100

    Eliminate function names and capitalize

--- bin/documentation.py
+++ bin/documentation.py
@@ -1,15 +1,28 @@
 import os, re, sys
 
+def clean(lines):
+    lines = [re.sub(r"^\*> ?", "", line) for line in lines]
+
+    while len(lines) > 0 and lines[0].strip() == "":
+        lines = lines[1:]
+
+    while len(lines) > 0 and lines[-1].strip() == "":
+        lines = lines[:-1]
+
+    if all(line.startswith("   ") or line.strip() == "" for line in lines):
+        lines = [re.sub(r"^   ", "", line) for line in lines]
+
+    return lines
+
 def extract(filename):
     SEARCHING = 0
     IN_PURPOSE = 1
     IN_DESCRIPTION = 2
 
-    state = SEARCHING
     lines = []
-
-    with open(filename) as f:
-        for line in f:
+    state = SEARCHING
+    with open(filename) as file:
+        for line in file:
             if state == SEARCHING:
                 if "par Purpose" in line:
                     state = IN_PURPOSE
@@ -21,35 +34,26 @@ def extract(filename):
                     break
                 lines.append(line.replace("\n", ""))
 
-    lines = [re.sub(r"^\*> ?", "", line) for line in lines]
+    return lines
 
-    while len(lines) > 0 and lines[0].strip() == "":
-        lines = lines[1:]
-    while len(lines) > 0 and lines[-1].strip() == "":
-        lines = lines[:-1]
-
-    if len(lines) == 0:
-        return []
-
-    if all(line.startswith("   ") or line.strip() == "" for line in lines):
-        lines = [re.sub(r"^   ", "", line) for line in lines]
-
-    result = [lines[0]]
-    for line in lines[1:]:
-        if line.startswith("   "):
-            result.extend(["```text", line, "```"])
-        else:
-            result.append(line)
-
-    return result
+def flush(line):
+    print("/// {}".format(line).strip())
 
 def print_documentation(f, reference):
     filename = os.path.join(reference, "BLAS", "SRC", "{}.f".format(f.name))
     if not os.path.exists(filename):
         return
-    lines = extract(filename)
-    names = [arg[0] for arg in f.args]
-    for line in lines:
-        for name in names:
-            line = re.sub(r"\b{}\b".format(name), r"`{}`".format(name), line)
-        print("/// {}".format(line).strip())
+
+    lines = clean(extract(filename))
+    if len(lines) == 0:
+        return
+
+    first = re.sub(r"(?i)\s*{}\s+".format(f.name), "", lines[0])
+    flush(first.strip().capitalize())
+    for line in lines[1:]:
+        if line.startswith("   "):
+            flush("```text")
+            flush(line[3:])
+            flush("```")
+        else:
+            flush(line)
