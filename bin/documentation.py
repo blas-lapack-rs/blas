a4964cc4ac6bae64dc7755f35ced0e54aeb8ff56
Ivan Ukhov <ivan.ukhov@gmail.com>
2016-11-20 16:07:33 +0100

    Split documentation comments into paragraphs

--- bin/documentation.py
+++ bin/documentation.py
@@ -1,7 +1,48 @@
 import os, re, sys
 
+class Blob:
+    def append(self, _):
+        pass
+
+    def finish(self, _, __):
+        pass
+
+class Formula(Blob):
+    def __init__(self):
+        self.lines = []
+
+    def append(self, line):
+        self.lines.append(line)
+
+    def format(self, output):
+        output.write("/// ```text\n")
+        for line in self.lines:
+            output.write("/// {}\n".format(line[3:]))
+        output.write("/// ```\n")
+
+class Space(Blob):
+    def format(self, output):
+        output.write("///\n")
+
+class Text(Blob):
+    def __init__(self):
+        self.lines = []
+
+    def append(self, line):
+        self.lines.append(line)
+
+    def finish(self, i, f):
+        if i == 0:
+            first = re.sub(r"(?i)\s*{}\s+".format(f.name), "", self.lines[0])
+            first = first.strip().capitalize()
+            self.lines[0] = first
+
+    def format(self, output):
+        for line in self.lines:
+            output.write("/// {}\n".format(line))
+
 def clean(lines):
-    lines = [re.sub(r"^\*> ?", "", line) for line in lines]
+    lines = [re.sub(r"^\*> ?", "", line.rstrip()) for line in lines]
 
     while len(lines) > 0 and lines[0].strip() == "":
         lines = lines[1:]
@@ -9,8 +50,13 @@ def clean(lines):
     while len(lines) > 0 and lines[-1].strip() == "":
         lines = lines[:-1]
 
-    if all(line.startswith("   ") or line.strip() == "" for line in lines):
-        lines = [re.sub(r"^   ", "", line) for line in lines]
+    margin = 0
+    for line in lines:
+        if len(line.strip()) > 0:
+            margin = min(margin, len(line) - len(line.strip()))
+    for i, line in enumerate(lines):
+        if len(line.strip()) > 0:
+            lines[i] = lines[i][margin:]
 
     return lines
 
@@ -36,24 +82,32 @@ def extract(filename):
 
     return lines
 
-def flush(line):
-    print("/// {}".format(line).strip())
+def partition(lines):
+    paragraphs = []
+    current = None
+
+    for line in lines:
+        if line.startswith("   "):
+            klass = Formula
+        elif len(line) == 0:
+            klass = Space
+        else:
+            klass = Text
+        if not isinstance(current, klass):
+            if current is not None:
+                paragraphs.append(current)
+            current = klass()
+        current.append(line)
+
+    if current is not None:
+        paragraphs.append(current)
+
+    return paragraphs
 
 def print_documentation(f, reference):
     filename = os.path.join(reference, "BLAS", "SRC", "{}.f".format(f.name))
     if not os.path.exists(filename):
         return
-
-    lines = clean(extract(filename))
-    if len(lines) == 0:
-        return
-
-    first = re.sub(r"(?i)\s*{}\s+".format(f.name), "", lines[0])
-    flush(first.strip().capitalize())
-    for line in lines[1:]:
-        if line.startswith("   "):
-            flush("```text")
-            flush(line[3:])
-            flush("```")
-        else:
-            flush(line)
+    for i, paragraph in enumerate(partition(clean(extract(filename)))):
+        paragraph.finish(i, f)
+        paragraph.format(sys.stdout)
