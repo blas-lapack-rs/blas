54be4e412fc5b167597aa28cf0dd7370e4fa8480
Ivan Ukhov <ivan.ukhov@gmail.com>
2017-07-08 16:21:31 +0200

    Update the examples

--- src/c.rs
+++ src/c.rs
@@ -10,8 +10,10 @@
 //! let b = vec![1.0, 5.0, 9.0, 2.0, 6.0, 10.0, 3.0, 7.0, 11.0, 4.0, 8.0, 12.0];
 //! let mut c = vec![2.0, 7.0, 6.0, 2.0, 0.0, 7.0, 4.0, 2.0];
 //!
-//! dgemm(Layout::ColumnMajor, Transpose::None, Transpose::None,
-//!       m, n, k, 1.0, &a, m, &b, k, 1.0, &mut c, m);
+//! unsafe {
+//!     dgemm(Layout::ColumnMajor, Transpose::None, Transpose::None,
+//!           m, n, k, 1.0, &a, m, &b, k, 1.0, &mut c, m);
+//! }
 //!
 //! assert_eq!(&c, &vec![40.0, 90.0, 50.0, 100.0, 50.0, 120.0, 60.0, 130.0]);
 //! ```
